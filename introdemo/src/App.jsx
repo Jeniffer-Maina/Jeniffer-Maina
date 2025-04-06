6d8ec7b219903075c10fd0553de19b2b0adb3ea6
Jeniffer-Maina <mainajeniffer221@gmail.com>
2025-04-06 15:04:33 +0000

    tailwind

---
+++ introdemo/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className="text-red-700 text-7xl bg-sky-200 text-center font-bold">This is Sparta</h1>
+      <p className="max-w-md mx-auto border p-6 m-6 text-blue-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis aliquid ea at ipsum mollitia nostrum, accusantium molestias maiores facilis quia ratione doloremque necessitatibus modi quasi! Est blanditiis et officiis similique?</p>
+    </>
+  )
+}
+
+export default App
